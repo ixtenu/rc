a90fe6ba014661ab6f0ff6a9ad382985b20104f0
ixtenu <ixtenu@pm.me>
2022-04-28 16:28:13 -0700

    emacs: add vertico
    Need something better than the default completion system.  Have used
    Ivy before, trying Vertico instead.  These use-package incantations
    are copy-pasted from the Vertico README.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -212,6 +212,47 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
   :bind (("C-x 1" . zygospore-toggle-delete-other-windows)))
 
 ;;
+;; Completion
+;;
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts.  Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations for Vertico...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  (if (not (version< emacs-version "28"))
+      (setq read-extended-command-predicate
+            #'command-completion-default-include-p))
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;;
 ;; User Utilities
 ;;
 
