e29aeb47966c80372c44a5f1776011c2648038f3
ixtenu <ixtenu@pm.me>
2022-06-04 03:22:19 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+scriptdir="$(cd -- "$(dirname "$0")" 2>&1 >/dev/null && pwd -P)"
+cd "$scriptdir"
+
+installfile() {
+	fn="$1"
+	fp="$(realpath $fn)"
+	od="$2"
+	lp="$od"/"$fn"
+	mkdir -p "$od"
+	if [ -e "$lp" ]; then
+		if [ -L "$lp" ]; then
+			ln -svf "$fp" "$lp"
+		else
+			echo "warning: $lp exists and is not a symbolic link, leaving it" 2>&1
+		fi
+	else
+		ln -sv "$fp" "$lp"
+	fi
+}
+
+if command -v X 2>&1 >/dev/null; then
+	installfile .Xresources "$HOME"
+fi
+if command -v cwm 2>&1 >/dev/null; then
+	installfile .cwmrc "$HOME"
+fi
+if command -v joe 2>&1 >/dev/null; then
+	installfile .joerc "$HOME"
+	installfile .jmacsrc "$HOME"
+fi
+if command -v mg 2>&1 >/dev/null; then
+	installfile .mg "$HOME"
+fi
+if command -v sam 2>&1 >/dev/null; then
+	# .samrc is for deadpixi/sam; don't install it for 9fans/plan9port sam
+	if [ -x /usr/local/bin/sam ]; then
+		installfile .samrc "$HOME"
+	fi
+fi
+if command -v zsh 2>&1 >/dev/null; then
+	installfile .zshrc "$HOME"
+fi
