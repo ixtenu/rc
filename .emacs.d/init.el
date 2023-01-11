4e599a855808566fa649e7aa55661f76ed8abbe5
ixtenu <ixtenu@pm.me>
2023-01-10 17:47:21 -0800

    emacs: make it easier to run shell commands
    Having been typing C-u M-| too often.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -323,6 +323,33 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
 (setq-default fill-column 80)
 (add-hook 'text-mode-hook 'auto-fill-mode)
 
+;; https://stackoverflow.com/a/207067
+(defun my-generalized-shell-command (command arg)
+  "Unifies `shell-command' and `shell-command-on-region'.  If no region is
+selected, run a shell command just like M-x shell-command (M-!).  If no region
+is selected and an argument is a passed, run a shell command and place its
+output after the mark as in C-u M-x `shell-command' (C-u M-!).  If a region is
+selected pass the text of that region to the shell and replace the text in that
+region with the output of the shell command as in C-u M-x
+`shell-command-on-region' (C-u M-|).  If a region is selected AND an argument is
+passed (via C-u) send output to another buffer instead of replacing the text in
+region."
+  (interactive (list (read-from-minibuffer "Shell command: " nil nil nil 'shell-command-history)
+                     current-prefix-arg))
+  (let ((p (if mark-active (region-beginning) 0))
+        (m (if mark-active (region-end) 0)))
+    (if (= p m)
+        ;; No active region
+        (if (eq arg nil)
+            (shell-command command)
+          (shell-command command t))
+      ;; Active region
+      (if (eq arg nil)
+          (shell-command-on-region p m command t t)
+        (shell-command-on-region p m command)))))
+
+(global-set-key (kbd "C-c \\") #'my-generalized-shell-command)
+
 (setq kill-whole-line t)
 (setq backward-delete-char-untabify-method 'hungry)
 
