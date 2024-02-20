246c04dc6644aa574ead148756edf8120083feec
ixtenu <ixtenu@pm.me>
2024-02-19 16:24:09 -0800

    Add .textadept
    For the Textadept text editor:
    <https://orbitalquark.github.io/textadept/index.html>

---
+++ .textadept/get-modules.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+set -eu
+
+scriptdir="$(cd -- "$(dirname $(readlink -f "$0"))" >/dev/null 2>&1 && pwd -P)"
+cd "$scriptdir"
+
+if [ -d modules ]; then
+	echo "$PWD/modules already exists: skipping download of modules ZIP file."
+	echo "Delete that directory if you want to reinitialize the Textadept modules."
+else
+	ta_ver="$(textadept --version | sed 1q | sed 's/Textadept //')"
+	curl -O -L https://github.com/orbitalquark/textadept/releases/download/textadept_${ta_ver}/textadept_${ta_ver}.modules.zip
+	unzip textadept_${ta_ver}.modules.zip
+	rm textadept_${ta_ver}.modules.zip
+	mv textadept-modules modules
+fi
+
+cd modules
+
+[ ! -d scratch ] && git clone https://github.com/orbitalquark/textadept-scratch.git scratch
+[ ! -d ctags ] && git clone https://github.com/orbitalquark/textadept-ctags.git ctags
+
+find . -maxdepth 2 -name '\.git' |
+while IFS= read -r g; do
+	pushd "$(dirname $g)"
+	git pull
+	popd
+done
---
+++ .textadept/init.lua
@@ -0,0 +1,43 @@
+-- Modules
+require('ctags')
+require('file_diff')
+local format = require('format')
+require('scratch')
+
+-- Font
+if not CURSES then view:set_theme{font = 'Hack', size = 13} end
+
+-- Highlight all instances of the current word
+textadept.editing.highlight_words = textadept.editing.HIGHLIGHT_CURRENT
+
+-- Increase maximum file list size (default is 5000)
+io.quick_open_max = 32768
+
+-- Always ensure final newline (this is false by default on Windows)
+io.ensure_final_newline = true
+
+-- Don't automatically pair quotation marks, parentheses, etc.
+textadept.editing.auto_pairs = nil
+
+-- Wrap lines at 80 characters
+format.line_length = 80
+
+-- Always strip trailing spaces, except in patch files.
+local function set_strip_trailing_spaces()
+	textadept.editing.strip_trailing_spaces = buffer.lexer_language ~= 'diff'
+end
+events.connect(events.LEXER_LOADED, set_strip_trailing_spaces)
+events.connect(events.BUFFER_AFTER_SWITCH, set_strip_trailing_spaces)
+events.connect(events.VIEW_AFTER_SWITCH, set_strip_trailing_spaces)
+
+-- Default indentation settings for all buffers.
+buffer.use_tabs = true
+buffer.tab_width = 8
+
+-- Indentation settings for individual languages.
+events.connect(events.LEXER_LOADED, function(name)
+        if name == 'python' or name == 'yaml' or name == 'zig' then
+                buffer.use_tabs = false
+                buffer.tab_width = 4
+        end
+end)
