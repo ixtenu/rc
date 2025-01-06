43aa2aba72890664e894ef5eeb31566731d9f669
ixtenu <ixtenu@pm.me>
2025-01-06 14:23:29 -0800

    vis: use colorcolumn as fillcolumn fallback

--- visrc.lua
+++ visrc.lua
@@ -158,13 +158,11 @@ vis:option_register("fc", "number", function(fc)
 	return true
 end, "Goal width for paragraph filling (gq)")
 
--- vim-like gq operator for wrapping text
+-- vim-like gq operator for rewrapping text
 vis:operator_new("gq", function(file, range, pos)
-	-- use fillcolumn if set, otherwise default to 80
 	local fc = vis.win.fillcolumn
-	if fc == nil or fc <= 0 then
-		fc = 80
-	end
+	if fc == nil or fc <= 0 then fc = vis.win.options.colorcolumn end
+	if fc == nil or fc <= 0 then fc = 80 end
 	-- fmts is a fmt(1) wrapper script that knows about comments, lists, etc.
 	-- see: https://github.com/ixtenu/script/blob/master/fmts
 	local status, out, err = vis:pipe(file, range, "fmts -w" .. fc)
@@ -175,7 +173,7 @@ vis:operator_new("gq", function(file, range, pos)
 		file:insert(range.start, out)
 	end
 	return range.start -- new cursor location
-end, "Formatting operator, filter range through fmt(1)")
+end, "Rewrap text operator")
 
 -- shortcuts for gq operator
 vis:map(vis.modes.NORMAL, 'Q', 'gqap')
