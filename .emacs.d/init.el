8f1bd541fae089bc658461e86926cfeff6962aec
ixtenu <ixtenu@pm.me>
2025-02-18 11:02:34 -0800

    emacs: indentation fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -422,14 +422,14 @@
 
     ;; Do not allow the cursor in the minibuffer prompt
     (setq minibuffer-prompt-properties
-          '(read-only t cursor-intangible t face minibuffer-prompt))
+      '(read-only t cursor-intangible t face minibuffer-prompt))
     (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
     ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
     ;; Vertico commands are hidden in normal buffers.
     (if (my-emacs-version>= "28")
-        (setq read-extended-command-predicate
-              #'command-completion-default-include-p))
+      (setq read-extended-command-predicate
+        #'command-completion-default-include-p))
 
     ;; Enable recursive minibuffers
     (setq enable-recursive-minibuffers t))
@@ -437,8 +437,8 @@
   (use-package orderless
     :init
     (setq completion-styles '(orderless basic)
-          completion-category-defaults nil
-          completion-category-overrides '((file (styles partial-completion))))))
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles partial-completion))))))
 
 
 ;;;; User Utilities:
